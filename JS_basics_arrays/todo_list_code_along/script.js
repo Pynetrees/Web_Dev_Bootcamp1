3c6526f7bfee79d979e9150abb9465b07dade4b0
John Lemuel Pyne IV <j.pyne.89@gmail.com>
2019-07-19 22:34:24 -0600

    Keep on Learning

--- JS_basics_arrays/todo_list_code_along/script.js
+++ JS_basics_arrays/todo_list_code_along/script.js
@@ -5,15 +5,36 @@ window.setTimeout(function () {
   while (input !== "quit") {
     //handle new input
     if (input === "list") {
-      console.log(todos);
+      listTodos();
     } else if (input === "new") {
-      //ask for new todo
-      var newTodo = prompt("Enter new todo");
-      //add to todos array
-      todos.push(newTodo);
+      addTodo();
+    } else if (input === "delete") {
+      deleteTodo();
     }
     //ask again for new input
     input = prompt("What would you like to do?");
   }
   console.log("ok, you quit the app")
+
+  function listTodos() {
+    console.log("**********");
+    todos.forEach(function (todo, i) {
+      console.log(i + ": " + todo);
+    })
+    console.log("**********");
+  }
+  function addTodo() {
+    //ask for new todo
+    var newTodo = prompt("Enter new todo");
+    //add to todos array
+    todos.push(newTodo);
+    console.log("Added Todo");
+  }
+  function deleteTodo() {
+    //ask for index of list item
+    var index = prompt("Enter index of todo to delete.");
+    //delete item
+    todos.splice(index, 1);
+    console.log("Deleted todo");
+  }
 }, 500);
