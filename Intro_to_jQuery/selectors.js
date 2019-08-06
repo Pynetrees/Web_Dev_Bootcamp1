441eac8e4a5e4d6fb6f24b2121d6af99a8c62c43
John Lemuel Pyne IV <j.pyne.89@gmail.com>
2019-08-06 11:17:16 -0600

    morning practice/learning jQuery

---
+++ Intro_to_jQuery/selectors.js
@@ -0,0 +1,7 @@
+$("div").css("backgroundColor", "purple");
+
+$(".highlight").css("width", "200px");
+
+$("#third").css("border", "10px solid orange");
+
+$("div:first-of-type").css("color", "pink");
---
+++ Todo_list_project/assets/js/todos.js
@@ -0,0 +1,5 @@
+//Check off Todos by clicking
+$("li").click(function () {
+  $(this).toggleClass("completed");
+});
+
