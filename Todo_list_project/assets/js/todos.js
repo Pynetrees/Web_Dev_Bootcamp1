8628d9e2b23b615c8865207421567658279e6e76
John Lemuel Pyne IV <j.pyne.89@gmail.com>
2019-08-07 12:02:11 -0600

    finished the to-do project before work

--- Todo_list_project/assets/js/todos.js
+++ Todo_list_project/assets/js/todos.js
@@ -1,5 +1,26 @@
 //Check off Todos by clicking
-$("li").click(function () {
+$("ul").on("click", "li", function () {
   $(this).toggleClass("completed");
 });
 
+//X Deletes Todo
+$("ul").on("click", "span", function (event) {
+  $(this).parent().fadeOut(500, function () {
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+$("input[type='text'").keypress(function (event) {
+  if (event.which === 13) {
+    //grabbing todo text from input
+    var todoText = $(this).val();
+    $(this).val("");
+    //create new li & add to ul
+    $("ul").append("<li><span><i class='fas fa-trash-alt'></i> </span>" + todoText + "</li>");
+  };
+});
+
+$("#toggle-form").click(function(){
+	$("input[type='text']").fadeToggle();
+});
