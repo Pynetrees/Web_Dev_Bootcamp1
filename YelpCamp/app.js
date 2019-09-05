dbb68219cd9fa806605ab0c85ee031a7be8ce8d6
John Lemuel Pyne IV <j.pyne.89@gmail.com>
2019-09-05 10:16:43 -0600

    morning practice

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,8 +1,17 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 var port = process.env.PORT || 3000;
+var campgrounds = [
+  { name: "Kite Lake", image: "/images/mountains-374429_1920.jpg" },
+  { name: "Boundary Waters", image: "/images/bwca-1150305_1920.jpg" },
+  { name: "Lost Creek", image: "/images/nature-1209302_1920.jpg" },
+  { name: "Buffalo Peaks", image: "/images/mountains-718911_1920.jpg" },
+  { name: "Acadia", image: "/images/acadia-national-park-80357_1920.jpg" }
+]
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
@@ -10,14 +19,20 @@ app.get("/", function (req, res) {
 });
 
 app.get("/campgrounds", function (req, res) {
-  var campgrounds = [
-    { name: "Kite Lake", image: "/images/mountains-374429_1920.jpg" },
-    { name: "Boundary Waters", image: "/images/bwca-1150305_1920.jpg" },
-    { name: "Lost Creek", image: "/images/nature-1209302_1920.jpg" },
-    { name: "Buffalo Peaks", image: "/images/mountains-718911_1920.jpg" },
-    { name: "Acadia", image: "/images/acadia-national-park-80357_1920.jpg" }
-  ]
+
   res.render("campgrounds", { campgrounds: campgrounds });
+});
+
+app.get("/campgrounds/new", function (req, res) {
+  res.render("new");
+});
+
+app.post("/campgrounds", function (req, res) {
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = { name: name, image: image };
+  campgrounds.push(newCampground);
+  res.redirect("/campgrounds");
 });
 
 app.listen(port, function () {
