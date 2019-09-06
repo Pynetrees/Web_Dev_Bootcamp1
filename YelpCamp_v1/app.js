3e6603b82de7d94a14b067ffdf50c1fdd0d5a49c
John Lemuel Pyne IV <j.pyne.89@gmail.com>
2019-09-06 16:33:18 -0600

    progress on Yelp Clone

--- YelpCamp_v1/app.js
+++ YelpCamp_v1/app.js
@@ -1,26 +1,49 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
-var port = process.env.PORT || 3000;
-var campgrounds = [
-  { name: "Kite Lake", image: "/images/mountains-374429_1920.jpg" },
-  { name: "Boundary Waters", image: "/images/bwca-1150305_1920.jpg" },
-  { name: "Lost Creek", image: "/images/nature-1209302_1920.jpg" },
-  { name: "Buffalo Peaks", image: "/images/mountains-718911_1920.jpg" },
-  { name: "Acadia", image: "/images/acadia-national-park-80357_1920.jpg" }
-]
+var express = require("express"),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  app = express(),
+  port = process.env.PORT || 3000
 
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({ 
+//   name: "Acadia", 
+//   image: "/images/acadia-national-park-80357_1920.jpg",
+//   description: "Huge island off coast of Maine great camping & tons of wildlife" }, function(err, campground)
+//   {
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log("NEWLY CREATED CAMPGROUND");
+//     console.log(campground);
+//   }
+// });
 
 app.get("/", function (req, res) {
   res.render("landing");
 });
 
 app.get("/campgrounds", function (req, res) {
-
-  res.render("campgrounds", { campgrounds: campgrounds });
+  //GET ALL CAMPGROUNDS FROM DB
+  Campground.find({}, function (err, allCampgrounds) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("campgrounds", { campgrounds: allCampgrounds });
+    }
+  })
 });
 
 app.get("/campgrounds/new", function (req, res) {
@@ -31,8 +54,14 @@ app.post("/campgrounds", function (req, res) {
   var name = req.body.name;
   var image = req.body.image;
   var newCampground = { name: name, image: image };
-  campgrounds.push(newCampground);
-  res.redirect("/campgrounds");
+  //CREATE NEW CAMPGROUND & SAVE 2 DATABASE
+  Campground.create(newCampground, function (err, newlyCreated) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/campgrounds");
+    }
+  })
 });
 
 app.listen(port, function () {
